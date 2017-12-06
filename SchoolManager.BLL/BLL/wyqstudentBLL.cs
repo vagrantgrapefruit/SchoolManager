927d32a1479f3282b225975296304826cda7237c
Shirly <1257350982@qq.com>
2017-12-06 16:05:32 +0800

    mmmm

---
+++ SchoolManager.BLL/BLL/wyqstudentBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolManager.Models;
+using System.Linq.Expressions;
+
+namespace SchoolManager.BLL
+{
+    public partial class wyqstudentBLL
+    {
+        public List<wyqstudentModel> GetList(Expression<Func<wyqstudent, bool>> whereLambda)
+        {
+            var query = m_Rep.GetList(whereLambda);
+            return CreateModelList(ref query);
+        }
+    }
+}
